63660bd67d8c9979edbd2c7f5e481abca6bbfc28
Certil Remy <certil@noukod.com>
2019-01-10 20:26:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,12 @@ class App < Sinatra::Base
   #erb :reversed
 
   @name = params[:name]
-  puts @name
-
-  puts params
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg= params[:sg]
+  @pf = params[:pf]
+  @sf = params[:sf]
+  @c = params[:c]
    end
 
 end
